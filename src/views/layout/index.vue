b89646090189de5133dd8340bf15a6db175437f0
Nikooooooooooo <82916991+NikoYoungdddd@users.noreply.github.com>
2023-06-14 16:48:27 +0800

    底部小改

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,16 +1,13 @@
 <template>
   <el-container class="layout-container">
-    <el-aside class="aside" width="auto"
-      ><AsideBar 
-      class="aside-menu"
-      :is-collapse="isCollapse"
-    />
+    <el-aside class="aside" width="auto"></el-aside>
+    <AsideBar class="aside-menu" :is-collapse="isCollapse" />
     </el-aside>
     <el-container>
-      <HeadBar @changeCollapse="changeCollapse"/>
+      <HeadBar @changeCollapse="changeCollapse" />
       <el-main style="height:100px;" class="main"><router-view /></el-main>
       <el-footer style="height:20px;font-size:12px;text-align:center;">
-        软件测试可视化平台@Copyright SSE 2021</el-footer>
+        软件测试可视化平台@Copyright SSE 2023 </el-footer>
     </el-container>
   </el-container>
 </template>
@@ -32,8 +29,8 @@ export default {
   },
   computed: {},
   watch: {},
-  created() {},
-  mounted() {},
+  created() { },
+  mounted() { },
   methods: {
     changeCollapse() {
       this.isCollapse = !this.isCollapse;
@@ -50,15 +47,18 @@ export default {
   top: 0;
   bottom: 0;
 }
+
 .aside {
   background-color: #d3dce6;
+
   .aside-menu {
     height: 100%;
   }
 }
+
 .main {
   background-color: #e9eef3;
 }
-.footer {
-}
+
+.footer {}
 </style>
